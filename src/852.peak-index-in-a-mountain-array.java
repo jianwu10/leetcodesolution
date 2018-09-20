bbb553bad43c171e1065f3bb6e7c72f879196d06
jianwu10 <jianwu10@gmail.com>
2018-09-20 16:23:10 -0700

    peak index in a mountain array

---
+++ src/852.peak-index-in-a-mountain-array.java
@@ -0,0 +1,79 @@
+/*
+ * [882] Peak Index in a Mountain Array
+ *
+ * https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+ *
+ * algorithms
+ * Easy (67.32%)
+ * Total Accepted:    27.4K
+ * Total Submissions: 40.6K
+ * Testcase Example:  '[0,1,0]'
+ *
+ * Let's call an array A a mountain if the following properties hold:
+ *
+ *
+ * A.length >= 3
+ * There exists some 0 < i < A.length - 1 such that A[0] < A[1] < ... A[i-1] <
+ * A[i] > A[i+1] > ... > A[A.length - 1]
+ *
+ *
+ * Given an array that is definitely a mountain, return any i such that A[0] <
+ * A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1].
+ *
+ * Example 1:
+ *
+ *
+ * Input: [0,1,0]
+ * Output: 1
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [0,2,1,0]
+ * Output: 1
+ *
+ *
+ * Note:
+ *
+ *
+ * 3 <= A.length <= 10000
+ * 0 <= A[i] <= 10^6
+ * A is a mountain, as defined above.
+ *
+ *
+ */
+class Solution {
+  public int peakIndexInMountainArray(int[] A) {
+    int start = 1;
+    int end = A.length - 2;
+
+    while (start + 1 < end) {
+      int mid = start + (end - start) / 2;
+      if (A[mid] > A[mid - 1]) {
+        start = mid;
+      } else {
+        end = mid;
+      }
+    }
+
+    if (isPeak(A, start)) {
+      return start;
+    }
+
+    if (isPeak(A, end)) {
+      return end;
+    }
+
+    return -1;
+  }
+
+  public boolean isPeak(int[] A, int index) {
+    if (A[index] > A[index - 1] && A[index] > A[index + 1]) {
+      return true;
+    }
+
+    return false;
+  }
+}
